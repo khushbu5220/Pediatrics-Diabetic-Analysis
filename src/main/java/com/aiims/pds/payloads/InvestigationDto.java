4281b3568e5da04a41bd89f4c2f46a81c3d0d6d8
Khushbu <singhkhushbu210@gmail.com>
2024-07-18 14:16:32 +0530

    change in dto

--- src/main/java/com/aiims/pds/payloads/InvestigationDto.java
+++ src/main/java/com/aiims/pds/payloads/InvestigationDto.java
@@ -3,12 +3,18 @@ package com.aiims.pds.payloads;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.aiims.pds.modals.CeliacSerology;
+import com.aiims.pds.modals.FootExamination;
+import com.aiims.pds.modals.FundusExamination;
 import com.aiims.pds.modals.HbA1cTable;
 import com.aiims.pds.modals.InvestigationTable;
+import com.aiims.pds.modals.LipidProfile;
+import com.aiims.pds.modals.UrineAlbuminCreatinineRatio;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -36,5 +42,9 @@ public class InvestigationDto
 	private String vitd;
 	private String remarks;
 	private List<HbA1cTable> hbA1ctable;
-	private List<InvestigationTable> investigationTable;
+	private CeliacSerology celiacSerology;
+	private UrineAlbuminCreatinineRatio urineAlbuminCreatinineRatio;
+	private FundusExamination fundusExamination;
+	private FootExamination footExamination;
+	private LipidProfile lipidProfile;
 }
